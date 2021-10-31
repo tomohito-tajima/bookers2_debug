4c9259ace2bdadd8ec5bebec1d55f8d3a9ad0716
tomohito-tajima <tomo322017@gmail.com>
2021-10-31 07:17:38 +0000

    Finish 8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except:[:top, :about]
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -10,6 +10,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path
   end
+
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
